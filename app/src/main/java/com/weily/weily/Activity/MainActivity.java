e5a8c8d656a857452f07969ab2182984b25b4299
Mystery00 <mystery0dyl520@gmail.com>
2016-10-17 13:18:19 +0800

    backup: to Zhu Qi: fragment

--- app/src/main/java/com/weily/weily/Activity/MainActivity.java
+++ app/src/main/java/com/weily/weily/Activity/MainActivity.java
@@ -3,8 +3,6 @@ package com.weily.weily.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,31 +13,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.weily.weily.Adapter.MyFragmentPagerAdapter;
 import com.weily.weily.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
 {
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
-        viewPager=(ViewPager)findViewById(R.id.viewpager);
         setSupportActionBar(toolbar);
-        String[] titles=getResources().getStringArray(R.array.main_fragment);
-        for (String title : titles)
-        {
-            tabLayout.addTab(tabLayout.newTab().setText(title));
-        }
-        MyFragmentPagerAdapter adapter=new MyFragmentPagerAdapter(getSupportFragmentManager(),this);
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
