e08f2017b02110bc024e6e5d02e78cefb805c920
Mystery0 <mystery0dyl@icloud.com>
2016-10-24 15:37:46 +0800

    backup; add login; layout finish;

--- app/src/main/java/com/weily/weily/Activity/SignInActivity.java
+++ app/src/main/java/com/weily/weily/Activity/SignInActivity.java
@@ -15,6 +15,9 @@ import android.widget.TextView;
 import com.weily.weily.PublicMethod.ExitApplication;
 import com.weily.weily.R;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SignInActivity extends AppCompatActivity
 {
     final private static String pattern_username = "[A-Za-z0-9]+";
@@ -23,7 +26,6 @@ public class SignInActivity extends AppCompatActivity
     private FloatingActionButton fab;
     private TextInputLayout username_layout;
     private TextInputLayout password_layout;
-    private TextView new_account;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -42,7 +44,6 @@ public class SignInActivity extends AppCompatActivity
         fab = (FloatingActionButton) findViewById(R.id.fab);
         username_layout=(TextInputLayout)findViewById(R.id.usernameLayout);
         password_layout=(TextInputLayout)findViewById(R.id.passwordLayout);
-        new_account=(TextView)findViewById(R.id.new_account);
 
         setSupportActionBar(toolbar);
     }
@@ -75,9 +76,12 @@ public class SignInActivity extends AppCompatActivity
             @Override
             public void afterTextChanged(Editable s)
             {
-                if(s.toString().length()<4||s.toString().length()>16)
+                if(!Pattern.compile("[A-Za-z0-9]+").matcher(s.toString()).matches())
                 {
-                    username_layout.setError("超出位数");
+                    username_layout.setError(getString(R.string.error_wrong_format));
+                }else if(s.toString().length()<4||s.toString().length()>20)
+                {
+                    username_layout.setError(getString(R.string.error_out_of_size_username));
                 }else
                 {
                     username_layout.setError(null);
@@ -89,19 +93,26 @@ public class SignInActivity extends AppCompatActivity
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after)
             {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count)
             {
-
             }
 
             @Override
             public void afterTextChanged(Editable s)
             {
-
+                if(!Pattern.compile("[A-Za-z0-9.]+").matcher(s.toString()).matches())
+                {
+                    password_layout.setError(getString(R.string.error_wrong_format));
+                }else if(s.toString().length()<6||s.toString().length()>16)
+                {
+                    password_layout.setError(getString(R.string.error_out_of_size_password));
+                }else
+                {
+                    password_layout.setError(null);
+                }
             }
         });
         fab.setOnClickListener(new View.OnClickListener()
@@ -109,10 +120,26 @@ public class SignInActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(isWrong())
+                {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }else
+                {
+                    Snackbar.make(view,"test",Snackbar.LENGTH_SHORT)
+                            .show();
+                }
             }
         });
     }
-
+    @SuppressWarnings("ConstantConditions")
+    private boolean isWrong()
+    {
+        return !Pattern.compile("[A-Za-z0-9]+").matcher(username_layout.getEditText().getText().toString()).matches()
+                ||!Pattern.compile("[A-Za-z0-9.]+").matcher(password_layout.getEditText().getText().toString()).matches()
+                ||username_layout.getEditText().getText().length()<4
+                ||username_layout.getEditText().getText().length()>20
+                ||password_layout.getEditText().getText().length()<6
+                ||password_layout.getEditText().getText().length()>16;
+    }
 }
