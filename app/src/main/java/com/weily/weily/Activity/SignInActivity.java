bce173639c8f1ae00b2d9f2940eb55a754f92d07
Mystery00 <mystery0dyl520@gmail.com>
2016-10-25 15:54:34 +0800

    backup;

--- app/src/main/java/com/weily/weily/Activity/SignInActivity.java
+++ app/src/main/java/com/weily/weily/Activity/SignInActivity.java
@@ -30,6 +30,7 @@ public class SignInActivity extends AppCompatActivity
     final private static String pattern_password = "[A-Za-z0-9.]+";
     final private static int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 222;
     private Toolbar toolbar;
+    private View view;
     private FloatingActionButton fab;
     private TextInputLayout username_layout;
     private TextInputLayout password_layout;
@@ -49,6 +50,7 @@ public class SignInActivity extends AppCompatActivity
     {
         ExitApplication.getInstance().addActivity(this);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        view=findViewById(R.id.coordinatorLayout);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         username_layout = (TextInputLayout) findViewById(R.id.usernameLayout);
         password_layout = (TextInputLayout) findViewById(R.id.passwordLayout);
@@ -128,42 +130,15 @@ public class SignInActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                if (!getSharedPreferences("permission",MODE_PRIVATE).getBoolean("WRITE_EXTERNAL_STORAGE",false))
+                if (ContextCompat.checkSelfPermission(SignInActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED)
                 {
                     //申请WRITE_EXTERNAL_STORAGE权限
                     ActivityCompat.requestPermissions(SignInActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                             WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
-                }
-                if (!isWrong())
+                }else
                 {
-                    final User user = new User();
-                    user.setUsername(username_layout.getEditText().getText().toString());
-                    user.setPassword(password_layout.getEditText().getText().toString());
-                    user.login(new SignInListener()
-                    {
-                        @Override
-                        public void Success()
-                        {
-                            //登陆成功保存登录信息
-                            SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
-                            sharedPreferences.edit()
-                                    .putString("username", username_layout.getEditText().getText().toString())
-                                    .putString("password", password_layout.getEditText().getText().toString())
-                                    .apply();
-                            finish();
-                        }
-
-                        @Override
-                        public void Failure(int code, String message)
-                        {
-                            Logs.loge(code, message);//打印错误信息
-                        }
-                    });
-                } else
-                {
-                    Logs.loge(getString(R.string.error_wrong_format));
-                    Snackbar.make(view, getString(R.string.error_wrong_format), Snackbar.LENGTH_SHORT)
-                            .show();
+                    login();
                 }
             }
         });
@@ -186,16 +161,50 @@ public class SignInActivity extends AppCompatActivity
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE)
         {
-            SharedPreferences sharedPreferences = getSharedPreferences("permission", MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
-                editor.putBoolean("WRITE_EXTERNAL_STORAGE", true);
-            } else
+                login();
+            }else
             {
-                editor.putBoolean("WRITE_EXTERNAL_STORAGE", false);
+                Snackbar.make(view,getString(R.string.error_permission_faild),Snackbar.LENGTH_LONG)
+                        .show();
             }
-            editor.apply();
+        }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void login()
+    {
+        if (!isWrong())
+        {
+            final User user = new User();
+            user.setUsername(username_layout.getEditText().getText().toString());
+            user.setPassword(password_layout.getEditText().getText().toString());
+            user.login(new SignInListener()
+            {
+                @Override
+                public void Success()
+                {
+                    //登陆成功保存登录信息
+                    SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
+                    sharedPreferences.edit()
+                            .putString("username", username_layout.getEditText().getText().toString())
+                            .putString("password", password_layout.getEditText().getText().toString())
+                            .apply();
+                    finish();
+                }
+
+                @Override
+                public void Failure(int code, String message)
+                {
+                    Logs.loge(code, message);//打印错误信息
+                }
+            });
+        } else
+        {
+            Logs.loge(getString(R.string.error_wrong_format));
+            Snackbar.make(view, getString(R.string.error_wrong_format), Snackbar.LENGTH_SHORT)
+                    .show();
         }
     }
 }
