eb3e8561f0a67d4ba98b3e2907a4d629bc696151
Mystery00 <mystery0dyl520@gmail.com>
2016-10-27 09:38:49 +0800

    backup: add share;

---
+++ app/src/main/java/com/weily/weily/Activity/SettingsActivity.java
@@ -0,0 +1,46 @@
+package com.weily.weily.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.weily.weily.PublicMethod.ExitApplication;
+import com.weily.weily.R;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        initialization();
+
+        monitor();
+    }
+
+    private void initialization()
+    {
+        ExitApplication.getInstance().addActivity(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void monitor()
+    {
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+            }
+        });
+    }
+
+}
