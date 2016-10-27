d6d0c976d415b39b77fbf1bff9f5240dd08320e9
Mystery00 <mystery0dyl520@gmail.com>
2016-10-27 09:11:17 +0800

    backup: add download; add usage; add splash;

--- app/src/main/java/com/weily/weily/Activity/ProfileActivity.java
+++ app/src/main/java/com/weily/weily/Activity/ProfileActivity.java
@@ -1,7 +1,11 @@
 package com.weily.weily.Activity;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -11,14 +15,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.weily.weily.Class.User;
+import com.weily.weily.PublicMethod.DownloadHeadFile;
 import com.weily.weily.PublicMethod.ExitApplication;
-import com.weily.weily.PublicMethod.Logs;
 import com.weily.weily.PublicMethod.SetStutes;
 import com.weily.weily.R;
 
 public class ProfileActivity extends AppCompatActivity
 {
+    final private static String HEADURL="head_url";
+    final private static String NAME="name";
+    final private static String COLLAGE="collage";
+    final private static String CLASS_NUMBER="class_number";
+    final private static String PHONE_NUMBER="phone_number";
+    final private static String PROFESSION="profession";
+    final private static String OCCUPATION="occupation";
+    final private static int DOWNLOAD=233;
     private Toolbar toolbar;
     private TextView show_name;
     private EditText show_collage;
@@ -27,6 +38,23 @@ public class ProfileActivity extends AppCompatActivity
     private EditText show_profession;
     private EditText show_occupation;
     private ImageView profile_background;
+
+    /**
+     * 解决子线程加载试图问题
+     */
+    @SuppressLint("HandlerLeak")
+    private Handler handler=new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            if(msg.what==DOWNLOAD)
+            {
+                    profile_background.setImageBitmap((Bitmap) msg.obj);
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -42,30 +70,13 @@ public class ProfileActivity extends AppCompatActivity
     {
         ExitApplication.getInstance().addActivity(this);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        show_name=(TextView)findViewById(R.id.text_name);
-        show_collage=(EditText)findViewById(R.id.text_collage);
-        show_class_number=(EditText)findViewById(R.id.text_class);
-        show_phone_number=(EditText)findViewById(R.id.text_phone);
-        show_profession=(EditText)findViewById(R.id.text_profession);
-        show_occupation=(EditText)findViewById(R.id.text_occupation);
-        profile_background=(ImageView)findViewById(R.id.profile_background);
-
-        profile_background.setImageResource(R.mipmap.nav_background);
-
-        Intent intent=getIntent();
-        User user= (User) intent.getBundleExtra("user").getSerializable("user");
-        if (user!=null)
-        {
-            show_name.setText(user.getName());
-            show_collage.setText(user.getCollege());
-            show_class_number.setText(user.getClassNumber());
-            show_phone_number.setText(user.getPhoneNumber());
-            show_profession.setText(user.getProfession());
-            show_occupation.setText(user.getOccupation());
-        }else
-        {
-            Logs.loge("获取为空!");
-        }
+        show_name = (TextView) findViewById(R.id.text_name);
+        show_collage = (EditText) findViewById(R.id.text_collage);
+        show_class_number = (EditText) findViewById(R.id.text_class);
+        show_phone_number = (EditText) findViewById(R.id.text_phone);
+        show_profession = (EditText) findViewById(R.id.text_profession);
+        show_occupation = (EditText) findViewById(R.id.text_occupation);
+        profile_background = (ImageView) findViewById(R.id.profile_background);
 
         setSupportActionBar(toolbar);
     }
@@ -85,6 +96,39 @@ public class ProfileActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        final SharedPreferences sharedPreferences=getSharedPreferences("user",MODE_PRIVATE);
+        show_name.setText(sharedPreferences.getString(NAME,""));
+        show_collage.setText(sharedPreferences.getString(COLLAGE,""));
+        show_class_number.setText(sharedPreferences.getString(CLASS_NUMBER,""));
+        show_phone_number.setText(sharedPreferences.getString(PHONE_NUMBER,""));
+        show_profession.setText(sharedPreferences.getString(PROFESSION,""));
+        show_occupation.setText(sharedPreferences.getString(OCCUPATION,""));
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Bitmap bitmap=DownloadHeadFile.getImage(sharedPreferences.getString(HEADURL,"http://git-sublime.github.io/test/weily/picture/logo.png"));
+                    Message message=new Message();
+                    message.what=DOWNLOAD;
+                    message.obj=bitmap;
+                    handler.sendMessage(message);
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.menu_profile, menu);
@@ -100,6 +144,8 @@ public class ProfileActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         switch (item.getItemId())
         {
             case R.id.action_edit:
@@ -113,9 +159,25 @@ public class ProfileActivity extends AppCompatActivity
                 toolbar.getMenu().findItem(R.id.action_edit).setVisible(false);
                 break;
             case R.id.action_logout:
+                //执行登出操作
+                editor.remove("username");
+                editor.remove("password");
+                editor.remove("name");
+                editor.remove("collage");
+                editor.remove("class_number");
+                editor.remove("phone_number");
+                editor.remove("profession");
+                editor.remove("occupation");
+                finish();
                 break;
             case R.id.action_done:
                 //执行更新个人资料操作
+                editor.putString("collage", show_collage.getText().toString());
+                editor.putString("class_number", show_class_number.getText().toString());
+                editor.putString("phone_number", show_phone_number.getText().toString());
+                editor.putString("profession", show_profession.getText().toString());
+                editor.putString("occupation", show_occupation.getText().toString());
+
                 SetStutes.unClick(show_collage);
                 SetStutes.unClick(show_class_number);
                 SetStutes.unClick(show_phone_number);
@@ -126,6 +188,7 @@ public class ProfileActivity extends AppCompatActivity
                 toolbar.getMenu().findItem(R.id.action_edit).setVisible(true);
                 break;
         }
+        editor.apply();
         return super.onOptionsItemSelected(item);
     }
 }
