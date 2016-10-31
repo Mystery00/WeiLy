58c6a4aefd5e1aaec3885c0ab19b9252b3657f69
Mystery00 <mystery0dyl520@gmail.com>
2016-10-31 15:43:57 +0800

    backup: add introduce

--- app/src/main/java/com/weily/weily/Fragment/IntroduceFragment/IntroduceDirectionFragment.java
+++ app/src/main/java/com/weily/weily/Fragment/IntroduceFragment/IntroduceDirectionFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.weily.weily.R;
 
@@ -13,6 +14,11 @@ public class IntroduceDirectionFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.content_introduce_direction,container,false);
+        View view=inflater.inflate(R.layout.content_introduce_direction,container,false);
+        RelativeLayout android_layout=(RelativeLayout)view.findViewById(R.id.android_layout);
+        RelativeLayout unity_layout=(RelativeLayout)view.findViewById(R.id.unity_layout);
+        RelativeLayout web_layout=(RelativeLayout)view.findViewById(R.id.web_layout);
+        RelativeLayout art_layout=(RelativeLayout)view.findViewById(R.id.art_layout);
+        return view;
     }
 }
