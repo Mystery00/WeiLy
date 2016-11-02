825a8cc2cbd0b546a34f478203beb097d1c8a5ce
Mystery0 <mystery0dyl@icloud.com>
2016-11-03 05:04:34 +0800

    backup: direction introduce finish;

--- app/src/main/java/com/weily/weily/Fragment/IntroduceFragment/IntroduceDirectionFragment.java
+++ app/src/main/java/com/weily/weily/Fragment/IntroduceFragment/IntroduceDirectionFragment.java
@@ -1,5 +1,8 @@
 package com.weily.weily.Fragment.IntroduceFragment;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.weily.weily.Activity.IntroduceAndroidActivity;
+import com.weily.weily.Activity.IntroduceArtActivity;
+import com.weily.weily.Activity.IntroduceUnityActivity;
+import com.weily.weily.Activity.IntroduceWebActivity;
 import com.weily.weily.R;
 
 public class IntroduceDirectionFragment extends Fragment
@@ -19,6 +26,62 @@ public class IntroduceDirectionFragment extends Fragment
         RelativeLayout unity_layout=(RelativeLayout)view.findViewById(R.id.unity_layout);
         RelativeLayout web_layout=(RelativeLayout)view.findViewById(R.id.web_layout);
         RelativeLayout art_layout=(RelativeLayout)view.findViewById(R.id.art_layout);
+        android_layout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                {
+                    startActivity(new Intent(getActivity(), IntroduceAndroidActivity.class), ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
+                }else
+                {
+                    startActivity(new Intent(getActivity(), IntroduceAndroidActivity.class));
+                }
+            }
+        });
+        unity_layout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                {
+                    startActivity(new Intent(getActivity(), IntroduceUnityActivity.class), ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
+                }else
+                {
+                    startActivity(new Intent(getActivity(), IntroduceUnityActivity.class));
+                }
+            }
+        });
+        web_layout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                {
+                    startActivity(new Intent(getActivity(), IntroduceWebActivity.class), ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
+                }else
+                {
+                    startActivity(new Intent(getActivity(), IntroduceWebActivity.class));
+                }
+            }
+        });
+        art_layout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                {
+                    startActivity(new Intent(getActivity(), IntroduceArtActivity.class), ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
+                }else
+                {
+                    startActivity(new Intent(getActivity(), IntroduceArtActivity.class));
+                }
+            }
+        });
         return view;
     }
 }
